b334f46850dd3525a5c30d58e13ca8b82ac6a49b
taku <taku.k@tcs.com>
2021-10-24 22:55:04 +0200

    Init

---
+++ MCDroid/Additions/GeofenceMessageResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Com.Salesforce.Marketingcloud.Messages.Geofence
+{
+    public partial class GeofenceMessageResponse
+    {
+        public global::Com.Salesforce.Marketingcloud.Location.LatLon RefreshCenter => InvokeRefreshCenter();
+
+        public int RefreshRadius => InvokeRefreshRadius();
+    }
+}
