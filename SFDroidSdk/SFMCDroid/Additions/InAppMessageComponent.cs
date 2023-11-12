c3c8d8a6b8a38959102121f9ac66a32f8a994d7e
taku k <takukatsi@gmail.com>
2023-11-12 12:54:03 +0200

    Android sdk fixed and tested.. its working

---
+++ SFDroidSdk/SFMCDroid/Additions/InAppMessageComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.Graphics;
+
+namespace Com.Salesforce.Marketingcloud.Messages.Iam
+{
+    public partial class InAppMessageComponent : global::Com.Salesforce.Marketingcloud.Messages.Iam.IInAppMessageManager, global::Com.Salesforce.Marketingcloud.Sfmcsdk.Components.Behaviors.IBehaviorListener
+    {
+        public void SetStatusBarColor(int p0)
+        {
+            this.StatusBarColor = p0;
+        }
+
+        public void SetTypeface(Typeface p0)
+        {
+            this.Typeface = p0;
+        }
+    }
+}
---
+++ SFDroidSdk/SFMCDroid/Additions/ProximityMessageResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Com.Salesforce.Marketingcloud.Messages.Proximity
+{
+    public partial class ProximityMessageResponse
+    {
+        public global::Com.Salesforce.Marketingcloud.Location.LatLon RefreshCenter => InvokeRefreshCenter();
+
+        public int RefreshRadius => InvokeRefreshRadius();
+    }
+}
